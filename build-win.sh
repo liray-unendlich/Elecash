bae8f94071188d979b54582292810ef82d7ac89a
liray-unendlich <unendlichkeit629@gmail.com>
2018-11-02 13:30:25 +0900

    Add build script to windows

---
+++ build-win.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git nsis
+
+sudo apt install -y g++-mingw-w64-x86-64
+
+PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g') # strip out problematic Windows %PATH% imported var
+cd ..
+chmod -R 755 Phore
+cd Phore
+cd depends
+make HOST=x86_64-w64-mingw32
+cd ..
+./autogen.sh
+CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --disable-tests --prefix=/build-win64
+make
+sudo make install
